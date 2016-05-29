0df8e059c33ce9a24c3643ea60decce2c7fb8571
Robin <robingiraudon@hotmail.fr>
2016-05-29 21:10:52 +0200

    Initial commit, fondation of the project.

---
+++ CryptoFun/Form_Main.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CryptoFun
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_Algo_Choice = new System.Windows.Forms.ComboBox();
+            this.textBox_In = new System.Windows.Forms.TextBox();
+            this.textBox_Out = new System.Windows.Forms.TextBox();
+            this.button_Encrypt = new System.Windows.Forms.Button();
+            this.button_Decrypt = new System.Windows.Forms.Button();
+            this.textBox_Key = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox_Algo_Choice
+            // 
+            this.comboBox_Algo_Choice.FormattingEnabled = true;
+            this.comboBox_Algo_Choice.Location = new System.Drawing.Point(32, 27);
+            this.comboBox_Algo_Choice.Name = "comboBox_Algo_Choice";
+            this.comboBox_Algo_Choice.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_Algo_Choice.TabIndex = 0;
+            this.comboBox_Algo_Choice.Visible = false;
+            // 
+            // textBox_In
+            // 
+            this.textBox_In.Location = new System.Drawing.Point(32, 89);
+            this.textBox_In.Multiline = true;
+            this.textBox_In.Name = "textBox_In";
+            this.textBox_In.Size = new System.Drawing.Size(100, 85);
+            this.textBox_In.TabIndex = 1;
+            // 
+            // textBox_Out
+            // 
+            this.textBox_Out.Location = new System.Drawing.Point(169, 89);
+            this.textBox_Out.Multiline = true;
+            this.textBox_Out.Name = "textBox_Out";
+            this.textBox_Out.Size = new System.Drawing.Size(100, 85);
+            this.textBox_Out.TabIndex = 2;
+            // 
+            // button_Encrypt
+            // 
+            this.button_Encrypt.Location = new System.Drawing.Point(32, 206);
+            this.button_Encrypt.Name = "button_Encrypt";
+            this.button_Encrypt.Size = new System.Drawing.Size(75, 23);
+            this.button_Encrypt.TabIndex = 3;
+            this.button_Encrypt.Text = "Encrypt";
+            this.button_Encrypt.UseVisualStyleBackColor = true;
+            this.button_Encrypt.Click += new System.EventHandler(this.button_Encrypt_Click);
+            // 
+            // button_Decrypt
+            // 
+            this.button_Decrypt.Location = new System.Drawing.Point(169, 206);
+            this.button_Decrypt.Name = "button_Decrypt";
+            this.button_Decrypt.Size = new System.Drawing.Size(75, 23);
+            this.button_Decrypt.TabIndex = 4;
+            this.button_Decrypt.Text = "Decrypt";
+            this.button_Decrypt.UseVisualStyleBackColor = true;
+            this.button_Decrypt.Click += new System.EventHandler(this.button_Decrypt_Click);
+            // 
+            // textBox_Key
+            // 
+            this.textBox_Key.Location = new System.Drawing.Point(169, 27);
+            this.textBox_Key.Name = "textBox_Key";
+            this.textBox_Key.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Key.TabIndex = 5;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.textBox_Key);
+            this.Controls.Add(this.button_Decrypt);
+            this.Controls.Add(this.button_Encrypt);
+            this.Controls.Add(this.textBox_Out);
+            this.Controls.Add(this.textBox_In);
+            this.Controls.Add(this.comboBox_Algo_Choice);
+            this.Name = "Main";
+            this.Text = "Crypto fun";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_Algo_Choice;
+        private System.Windows.Forms.TextBox textBox_In;
+        private System.Windows.Forms.TextBox textBox_Out;
+        private System.Windows.Forms.Button button_Encrypt;
+        private System.Windows.Forms.Button button_Decrypt;
+        private System.Windows.Forms.TextBox textBox_Key;
+    }
+}
+
---
+++ CryptoFun/Form_Main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CryptoFun
+{
+    public partial class Main : Form
+    {
+        enum CryptoAlgorithme {CA_Ceasar};
+        private CryptoAlgorithme Chosen_CryptoAlgorithme ;
+        public Main()
+        {
+            InitializeComponent();
+            InitializeValues();
+        }
+
+        private void InitializeValues()
+        {
+            Chosen_CryptoAlgorithme = CryptoAlgorithme.CA_Ceasar;
+        }
+
+       private String Ceasar_Encrypt(String Source_Text, int Key)
+        {
+            int Encoded_Int;
+            String Result = "";   
+            //UnicodeEncoding       
+            foreach (char Character in Source_Text)
+            {
+                Encoded_Int=((int)Character) + Key;
+                Result = Result + (((char)Encoded_Int)%(System.Char.MaxValue));               
+            }
+
+            return Result;
+        }
+
+        private void button_Encrypt_Click(object sender, EventArgs e)
+        {
+            String Encoded_Text="";
+
+            switch (Chosen_CryptoAlgorithme)
+            {
+                case CryptoAlgorithme.CA_Ceasar:     
+                    //if Ceasar_Key_Conform then
+                    Encoded_Text = Ceasar_Encrypt(textBox_In.Text, Int32.Parse(textBox_Key.Text));
+                    break;                    
+            }
+
+            textBox_Out.Text = Encoded_Text;
+        }
+
+        private void button_Decrypt_Click(object sender, EventArgs e)
+        {
+            String Decoded_Text = "";
+
+            switch (Chosen_CryptoAlgorithme)
+            {
+                case CryptoAlgorithme.CA_Ceasar:
+                    //if Ceasar_Key_Conform then
+                    Decoded_Text = Ceasar_Encrypt(textBox_Out.Text, -Int32.Parse(textBox_Key.Text));
+                    break;
+            }
+
+            textBox_In.Text = Decoded_Text;
+        }
+    }
+}
