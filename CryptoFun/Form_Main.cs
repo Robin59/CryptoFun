acc6c59e6bf7d1bcded1f0fe5d57fad3154eaa2b
Robin <robingiraudon@hotmail.fr>
2016-07-03 22:51:48 +0200

    refactoring : use of class for the differents cypher algorithm

--- CryptoFun/Form_Main.cs
+++ CryptoFun/Form_Main.cs
@@ -10,10 +10,58 @@ using System.Windows.Forms;
 
 namespace CryptoFun
 {
+    
     public partial class Main : Form
     {
-        enum CryptoAlgorithme {CA_Ceasar};
-        private CryptoAlgorithme Chosen_CryptoAlgorithme ;
+        enum CryptoAlgorithm { CA_Ceasar };
+
+        private abstract class Cypher_Algorithm
+        {
+          
+            protected abstract bool Key_Conform(String Key);
+            protected abstract String Encrypt_Specific_Algo(String Source_Text, String Key);
+            protected abstract String Decrypt_Specific_Algo(String Cypher_Text, String Key);
+
+            public String Encrypt(String Source_Text, String Key)
+            {
+                if (Key_Conform(Key)) 
+                    return Encrypt_Specific_Algo(Source_Text, Key);                
+                    else return "";
+            }
+            public String Decrypt(String Cypher_Text, String Key)
+            {
+                if (Key_Conform(Key))
+                    return Decrypt_Specific_Algo(Cypher_Text, Key);
+                    else return "";
+            }
+        }
+
+        private class Ceasar_Cypher : Cypher_Algorithm
+        {
+            protected override bool Key_Conform(String Key) { return true; }
+
+            protected override String Encrypt_Specific_Algo(String Source_Text, String Key)
+            {
+                int Encoded_Int;
+                String Result = "";
+                foreach (char Character in Source_Text)
+                {
+                    Encoded_Int = ((int)Character) + int.Parse(Key);
+                    Result = Result + (char)((Encoded_Int) % (System.Char.MaxValue));
+                }
+
+                return Result;
+            }
+
+            protected override String Decrypt_Specific_Algo(String Cypher_Text, String Key)
+            {
+                return Encrypt_Specific_Algo(Cypher_Text, "-"+Key);
+            }
+           
+        }
+       
+        private Cypher_Algorithm Chosen_CryptoAlgorithm;
+
         public Main()
         {
             InitializeComponent();
@@ -22,51 +70,18 @@ namespace CryptoFun
 
         private void InitializeValues()
         {
-            Chosen_CryptoAlgorithme = CryptoAlgorithme.CA_Ceasar;
+            Chosen_CryptoAlgorithm = new Ceasar_Cypher();
         }
-
-       private String Ceasar_Encrypt(String Source_Text, int Key)
-        {
-            int Encoded_Int;
-            String Result = "";   
-            //UnicodeEncoding       
-            foreach (char Character in Source_Text)
-            {
-                Encoded_Int=((int)Character) + Key;
-                Result = Result + (((char)Encoded_Int)%(System.Char.MaxValue));               
-            }
-
-            return Result;
-        }
+               
 
         private void button_Encrypt_Click(object sender, EventArgs e)
         {
-            String Encoded_Text="";
-
-            switch (Chosen_CryptoAlgorithme)
-            {
-                case CryptoAlgorithme.CA_Ceasar:     
-                    //if Ceasar_Key_Conform then
-                    Encoded_Text = Ceasar_Encrypt(textBox_In.Text, Int32.Parse(textBox_Key.Text));
-                    break;                    
-            }
-
-            textBox_Out.Text = Encoded_Text;
+            textBox_Out.Text = Chosen_CryptoAlgorithm.Encrypt(textBox_In.Text, textBox_Key.Text);
         }
 
         private void button_Decrypt_Click(object sender, EventArgs e)
-        {
-            String Decoded_Text = "";
-
-            switch (Chosen_CryptoAlgorithme)
-            {
-                case CryptoAlgorithme.CA_Ceasar:
-                    //if Ceasar_Key_Conform then
-                    Decoded_Text = Ceasar_Encrypt(textBox_Out.Text, -Int32.Parse(textBox_Key.Text));
-                    break;
-            }
-
-            textBox_In.Text = Decoded_Text;
+        {            
+            textBox_In.Text = Chosen_CryptoAlgorithm.Decrypt(textBox_Out.Text, textBox_Key.Text);
         }
     }
 }
