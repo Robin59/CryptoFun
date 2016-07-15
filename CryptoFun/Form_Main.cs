308441347057daad72fa0b40b58f28db08618d12
Robin <robingiraudon@hotmail.fr>
2016-07-15 16:38:20 +0200

    The feistel algorithm is now implemented for the DES

--- CryptoFun/Form_Main.cs
+++ CryptoFun/Form_Main.cs
@@ -11,28 +11,28 @@ using System.Windows.Forms;
 
 namespace CryptoFun
 {
-    
+
     public partial class Main : Form
-    {       
+    {
 
         private abstract class Cypher_Algorithm
         {
-          
+
             protected abstract bool Key_Conform(String Key);
             protected abstract String Encrypt_Specific_Algo(String Source_Text, String Key);
             protected abstract String Decrypt_Specific_Algo(String Cypher_Text, String Key);
 
             public String Encrypt(String Source_Text, String Key)
             {
-                if (Key_Conform(Key)) 
-                    return Encrypt_Specific_Algo(Source_Text, Key);                
-                    else return "The key is incompatible with the chosen algorithm";
+                if (Key_Conform(Key))
+                    return Encrypt_Specific_Algo(Source_Text, Key);
+                else return "The key is incompatible with the chosen algorithm";
             }
             public String Decrypt(String Cypher_Text, String Key)
             {
                 if (Key_Conform(Key))
                     return Decrypt_Specific_Algo(Cypher_Text, Key);
-                    else return "The key is incompatible with the chosen algorithm";
+                else return "The key is incompatible with the chosen algorithm";
             }
         }
 
@@ -41,7 +41,7 @@ namespace CryptoFun
             protected override bool Key_Conform(String Key)
             {
                 Int32 Mook_Int;
-                return Int32.TryParse(Key, out Mook_Int) && (Mook_Int>=0);
+                return Int32.TryParse(Key, out Mook_Int) && (Mook_Int >= 0);
             }
 
             protected override String Encrypt_Specific_Algo(String Source_Text, String Key)
@@ -59,45 +59,78 @@ namespace CryptoFun
 
             protected override String Decrypt_Specific_Algo(String Cypher_Text, String Key)
             {
-                return Encrypt_Specific_Algo(Cypher_Text, "-"+Key);
+                return Encrypt_Specific_Algo(Cypher_Text, "-" + Key);
             }
 
-            public override String ToString() {return "Ceasar cypher";}
-           
+            public override String ToString() { return "Ceasar cypher"; }
+
         }
         // End of the Ceasar cypher\\
 
         private class DES_Cypher : Cypher_Algorithm
         {
-            const int length_Key_in_Bits=64;
+            const int length_Key_in_Bits = 64;
+            private byte[] PI = { 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7 };
+            private byte[] IP = { 40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25 };
+            private byte[] E = { 32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1 };
+            private byte[] FP = { 16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25 };
+            //The S-boxes S[0] represent S1, S[1] is S2 etc. 
+            private byte[,,] S = { { { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 }, { 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 }, { 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 }, { 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 } }, { { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 }, { 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 }, { 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 }, { 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 } }, { { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8 }, { 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 }, { 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 }, { 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 } }, { { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 }, { 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 }, { 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 }, { 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 } }, { { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 }, { 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 }, { 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 }, { 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 } }, { { 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 }, { 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 }, { 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 }, { 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 } }, { { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 }, { 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 }, { 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 }, { 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 } }, { { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 }, { 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 }, { 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 }, { 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } } };
 
             protected override bool Key_Conform(String Key)
-            {   
+            {
                 byte[] Key_in_Bytes = System.Text.UTF32Encoding.Default.GetBytes(Key);
                 BitArray Key_in_Bits = new System.Collections.BitArray(Key_in_Bytes);
-                return Key_in_Bits.Length==length_Key_in_Bits;
+                return Key_in_Bits.Length == length_Key_in_Bits;
             }
-            
-            ///<param name="Message">Message in clear, must be 64 bits long</param> 
+
+            ///<param name="L">left parts of the message, 32 bits long</param> 
+            ///<param name="R">Rigth parts of the message, 32 bits long</param> 
             ///<param name="Ki">The partial keys, need 16 keys of 48 bits long</param>                        
-            private void DES_Algo(BitArray Message, BitArray[] Ki )
-            {
-                int[] PI = { 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7 };
-                int[] IP = { 40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25 };
+            private void Feistel_Algo(BitArray L, BitArray R, BitArray[] Ki)
+            {                                                                
+                for (int i=0; i<Ki.Length; i++)
+                {
+                    
+                    BitArray ExpendedR = new BitArray(48);
+                    for (int j = 0; j < 48; j++) ExpendedR[j] = R[(E[j] - 1)]; // Expension fonction, R goes from 32 bits to 48
 
-                Boolean[] L = new Boolean[Message.Length / 2];
-                Boolean[] R = new Boolean[Message.Length / 2];
+                    ExpendedR.And(Ki[i]);// Add the partial key ki to the rigth part
 
-                // Initial permutation on the message IP
-                for (int i = 0; i < (Message.Length/2); i++)
-                    L[i]=Message[PI[i] - 1];
-                for (int i=(Message.Length/2); i < (Message.Length); i++)
-                    R[i-(Message.Length/2)] = Message[PI[i] - 1];
+                    // the transformed left part is cut in 8 parts of 6 bits 
+                    byte[] SBoxesResult = new byte[4]; //Since the 8 results of the S-Boxes are 4 bits long, we're putting two results in one byte
+                    for (int j=0; j<8; j++)
+                    {
+                        // for each of the 8 parts, the first bit plus the last one decide the ordinate, the middle bits define the abscissa
+                        int x = ExpendedR[j*6] ? 0 : 2; 
+                        x = ExpendedR[j*6+5] ? x : x + 1;
+                        int y = ExpendedR[j*6+1] ? 0 : 8;
+                        y = ExpendedR[j*6+2] ? y : y+4;
+                        y = ExpendedR[j*6+3] ? y : y+2;
+                        y = ExpendedR[j*6+4] ? y : y+1;
 
-                //Main part : Feistel algo 
+                        //Since the 8 results of the S-Boxes are 4 bits long, we're putting two results in one byte
+                        SBoxesResult[j/2] = (j % 2==0)? S[j, x, y] : (byte)(SBoxesResult[j/2]+S[j, x, y]*16);
+                    }
+                         
+                    BitArray SBoxesResult_Bits = new BitArray(SBoxesResult);
 
-                // inverse permutation on the message IP^(-1)     
+                    //final permutation for to have the result of the fonction f
+                    BitArray f = new BitArray (SBoxesResult_Bits.Length);
+                    for (int j = 0; j < SBoxesResult_Bits.Length; j++)
+                        f[j] = SBoxesResult_Bits[(FP[j]-1)];
+
+                    L.And(f);
+
+                    //left part and rigth part change positions
+                    BitArray temp_left = L;//new BitArray(L);
+                    L = R;
+                    R = temp_left;
+                }
+                
             }
+
+
 
             ///<param name="Key">The key must be 64 bits long, even if only 56 bits of it are use</param>                        
             private BitArray[] Partial_Keys_Creation(BitArray Key)
@@ -164,11 +197,22 @@ namespace CryptoFun
                 TempoBytes = System.Text.UTF32Encoding.Default.GetBytes(Source_Text);
                 BitArray Source_Text_in_Bits = new System.Collections.BitArray(TempoBytes);
                 // dived message in part of 64 bits
+                // TO DO 
 
-                //DES_Algo(Source_Text_in_Bits, Ki);
+                // Initial permutation PI on the message + split it in two parts, the left part and the rigth part
+                BitArray L = new BitArray(Source_Text_in_Bits.Length / 2);
+                BitArray R = new BitArray(Source_Text_in_Bits.Length / 2);                
+                for (int i = 0; i < (Source_Text_in_Bits.Length / 2); i++)
+                    L[i] = Source_Text_in_Bits[PI[i] - 1];
+                for (int i = (Source_Text_in_Bits.Length / 2); i < (Source_Text_in_Bits.Length); i++)
+                    R[i - (Source_Text_in_Bits.Length / 2)] = Source_Text_in_Bits[PI[i] - 1];
 
+                Feistel_Algo(L,R,Ki);
+
+                // aggregate the left and rigth parts + inverse permutation IP^(-1) on the coded message
+                //to do 
                 // convert bits to String
-
+                //to do 
                 return "Not working for now";
             }
 
