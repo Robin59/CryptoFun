0cc29d876b905f499db701bfc9d397a92b300c65
Robin <robingiraudon@hotmail.fr>
2016-08-12 15:00:57 -0700

    Bug correction : The way .net encrypt bytes[] to BitArray was not compatible with the DES algo, it has been corrected (but there is still bugs remaining for DES)

--- CryptoFun/Form_Main.cs
+++ CryptoFun/Form_Main.cs
@@ -70,12 +70,27 @@ namespace CryptoFun
         private class DES_Cypher : Cypher_Algorithm
         {
             const int length_Key_in_Bits = 64;
-            private byte[] PI = { 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7 };
-            private byte[] IP = { 40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25 };
+            private byte[] IP = { 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7 };
+            private byte[] FP = { 40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25 };
             private byte[] E = { 32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1 };
-            private byte[] FP = { 16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25 };
+            private byte[] P = { 16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25 };
             //The S-boxes S[0] represent S1, S[1] is S2 etc. 
             private byte[,,] S = { { { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 }, { 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 }, { 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 }, { 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 } }, { { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 }, { 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 }, { 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 }, { 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 } }, { { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8 }, { 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 }, { 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 }, { 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 } }, { { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 }, { 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 }, { 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 }, { 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 } }, { { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 }, { 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 }, { 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 }, { 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 } }, { { 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 }, { 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 }, { 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 }, { 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 } }, { { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 }, { 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 }, { 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 }, { 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 } }, { { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 }, { 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 }, { 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 }, { 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } } };
+
+            /// <summary>
+            /// inverse the position of the most and least significant bit in the bit array
+            /// ex : 0100000 become 00000010
+            /// </summary>
+            /// <param name="aBitArray"></param>
+            private void change_Least_Significant_Bit_position(BitArray aBitArray)
+            {
+                BitArray tempBitArray = new BitArray(aBitArray);
+                for (int i = 0; i < aBitArray.Length; i++)
+                {
+                    int x = (i / 8) * 8 + (7 - i % 8);
+                    aBitArray[i] = tempBitArray[x];
+                }
+            }
 
             protected override bool Key_Conform(String Key)
             {
@@ -110,18 +125,16 @@ namespace CryptoFun
                         y = ExpendedR[j*6+4] ? y : y+1;
 
                         //Since the 8 results of the S-Boxes are 4 bits long, we're putting two results in one byte
-                        //SBoxesResult[j/2] = (j % 2==0)? S[j, x, y] : (byte)(SBoxesResult[j/2]+S[j, x, y]*16);
-                        if (j % 2 == 0)
-                            SBoxesResult[j / 2]= S[j, x, y];
-                       else SBoxesResult[j / 2]= (byte)(SBoxesResult[j / 2] + (S[j, x, y]*16));
+                        SBoxesResult[j/2] = (j % 2==0)? S[j, x, y] : (byte)(SBoxesResult[j/2]+S[j, x, y]*16);                       
                     }
                          
                     BitArray SBoxesResult_Bits = new BitArray(SBoxesResult);
+                    change_Least_Significant_Bit_position(SBoxesResult_Bits);
 
                     //final permutation for to have the result of the fonction f
                     BitArray f = new BitArray (SBoxesResult_Bits.Length);
                     for (int j = 0; j < SBoxesResult_Bits.Length; j++)
-                        f[j] = SBoxesResult_Bits[(FP[j]-1)];
+                        f[j] = SBoxesResult_Bits[(P[j]-1)];
 
                     L.And(f);
 
@@ -131,9 +144,7 @@ namespace CryptoFun
                     R = temp_left;
                 }
                 
-            }
-
-
+            }        
 
             ///<param name="Key">The key must be 64 bits long, even if only 56 bits of it are use</param>                        
             private BitArray[] Partial_Keys_Creation(BitArray Key)
@@ -190,7 +201,7 @@ namespace CryptoFun
             }
 
             protected override String Encrypt_Specific_Algo(String Source_Text, String Key)
-            {
+            {               
                 return Crypt_Decrypt_Specific_Algo(Source_Text,Key, false);
             }
 
@@ -207,6 +218,7 @@ namespace CryptoFun
                 String Coded_text_String = "";
                 byte[] TempoBytes = System.Text.Encoding.Default.GetBytes(Key);
                 BitArray Key_in_Bits = new System.Collections.BitArray(TempoBytes);
+                change_Least_Significant_Bit_position(Key_in_Bits);// change MSB and LSB positions to make it more conveniant for the rest of the algo
                 BitArray[] Ki = Partial_Keys_Creation(Key_in_Bits);
                 if (encrypt) Array.Reverse(Ki);//the order of the keys are change if we want to decrypt
 
@@ -214,6 +226,7 @@ namespace CryptoFun
                     Text += " ";
                 TempoBytes = System.Text.Encoding.Default.GetBytes(Text);
                 BitArray Source_Text_in_Bits = new System.Collections.BitArray(TempoBytes);
+                change_Least_Significant_Bit_position(Source_Text_in_Bits);
                 // divid message in part of 64 bits
                 for (int i = 0; i < Source_Text_in_Bits.Length / Message_Blocks_Lenght; i++)
                 {
@@ -221,20 +234,21 @@ namespace CryptoFun
                     BitArray L = new BitArray(Message_Blocks_Lenght / 2);
                     BitArray R = new BitArray(Message_Blocks_Lenght / 2);
                     for (int j = 0; j < (Message_Blocks_Lenght / 2); j++)
-                        L[j] = Source_Text_in_Bits[PI[j] - 1 + i * 64];
+                        L[j] = Source_Text_in_Bits[IP[j] - 1 + i * 64];
                     for (int j = (Message_Blocks_Lenght / 2); j < (Message_Blocks_Lenght); j++)
-                        R[j - (Message_Blocks_Lenght / 2)] = Source_Text_in_Bits[PI[j] - 1 + i * 64];
+                        R[j - (Message_Blocks_Lenght / 2)] = Source_Text_in_Bits[IP[j] - 1 + i * 64];
 
                     Feistel_Algo(L, R, Ki);
 
-                    // aggregate the left and rigth parts + inverse permutation IP^(-1) on the coded message
+                    // aggregate the left and rigth parts + final permutation IP^(-1) on the coded message
                     BitArray Coded_text_In_Bits = new BitArray(64);
-                    for (int j = 0; j < IP.Length; j++)
-                        Coded_text_In_Bits[j] = (IP[j] - 1) < L.Length ? L[IP[j] - 1] : R[IP[j] - 1 - L.Length];
+                    for (int j = 0; j < FP.Length; j++)
+                        Coded_text_In_Bits[j] = (FP[j] - 1) < L.Length ? L[FP[j] - 1] : R[FP[j] - 1 - L.Length];
 
                     // convert bits to String  
                     byte[] Coded_text_In_Bytes = new byte[Coded_text_In_Bits.Length / 8];
-                    Coded_text_In_Bits.CopyTo(Coded_text_In_Bytes, 0);
+                    change_Least_Significant_Bit_position(Coded_text_In_Bits);//Put back the MSB and LSB in the order .Net is use to
+                    Coded_text_In_Bits.CopyTo(Coded_text_In_Bytes, 0);                    
                     Coded_text_String = Coded_text_String + System.Text.Encoding.Default.GetString(Coded_text_In_Bytes);
                 }
                 
